d2fdc3dc0c5a1affa8d192590a2de393fd1d606f
Simone Amorim <simoneas03@gmail.com>
2019-09-08 19:06:17 -0300

    create crud for project module

--- src/app/controllers/projectController.js
+++ src/app/controllers/projectController.js
@@ -10,15 +10,37 @@ const router = express.Router();
 router.use(authMiddleware);
 
 router.get("/", async (req, res) => {
-  res.send({ user: req.userId });
+  try {
+    const projects = await Project.find().populate("user");
+
+    return res.send({ projects });
+  } catch (err) {
+    return res.status(400).send({ error: `Error ${err} to loading projects` });
+  }
 });
 
 router.get("/:projectId", async (req, res) => {
-  res.send({ user: req.userId });
+  try {
+    const project = await Project.findById(req.params.projectId).populate(
+      "user"
+    );
+
+    return res.send({ project });
+  } catch (err) {
+    return res.status(400).send({ error: `Error ${err} to loading project` });
+  }
 });
 
 router.post("/", async (req, res) => {
-  res.send({ user: req.userId });
+  try {
+    const project = await Project.create({ ...req.body, user: req.userId });
+
+    return res.send({ project });
+  } catch (err) {
+    return res
+      .status(400)
+      .send({ error: `Error ${err} to creatre a new project` });
+  }
 });
 
 router.put("/:projectId", async (req, res) => {
@@ -26,7 +48,13 @@ router.put("/:projectId", async (req, res) => {
 });
 
 router.delete("/:projectId", async (req, res) => {
-  res.send({ user: req.userId });
+  try {
+    await Project.findByIdAndRemove(req.params.projectId);
+
+    return res.send();
+  } catch (err) {
+    return res.status(400).send({ error: `Error ${err} to deleting project` });
+  }
 });
 
 module.exports = app => app.use("/projects", router);
